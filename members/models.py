0cbe1f0827c9d3bca086bf8aefffd764d325c66d
Bk201li <bk201li@protonmail.com>
2022-05-05 16:38:20 +0200

    🚧 work in progress

--- members/models.py
+++ members/models.py
@@ -1,38 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Group
 
 # Create your models here.
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
-    def create_user(self, email, password=None, is_admin=False, is_staff=False, is_active=True):       
-        user = self.model(
-            email=self.normalize_email(email)
-        )
 
-        if password:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-        user.admin = is_admin
-        user.staff = is_staff
-        user.active = is_active
-        user.save(using=self._db)
-        return user
-        
-    def create_superuser(self, email, password, **extra_fields):
-        user = self.model(
-            email=self.normalize_email(email)
-        )
-
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be set.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.admin = True
-        user.staff = True
-        user.active = True
-        user.save(using=self._db)
+        user.save()
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        user = self._create_user(email, password, **extra_fields)
+        admin_group, _ = Group.objects.get_or_create(name="admin")
+        user.groups.add(admin_group)
+        user.save()
         return user
 
 class User(AbstractUser):
+    objects = UserManager()
     first_name = models.CharField(max_length=255, blank=True, default='')
     last_name = models.CharField(max_length=255, blank=True, default='')
     birth_date = models.DateField(null=True)
@@ -47,7 +50,6 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    objects = UserManager()
 
     def __str__(self):
         return self.email
--- members/serializers.py
+++ members/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'email', 'password', 'first_name', 'last_name', 'birth_date', 'phone_number', 'image', 'area', 'created_at', 'updated_at']
         extra_kwargs = {
             'password': {
                 'write_only': True,
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = self.Meta.model.create_user(**validated_data)
+        instance = User.objects.create_user(**validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
