2e77dec53976338246d995445e9ba14ce1718f84
Bk201li <bk201li@protonmail.com>
2022-05-08 23:31:44 +0200

    images working

--- encheres/settings/__init__.py
+++ encheres/settings/__init__.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a6gc*&8)r+w$2e03)o2@m$&u_t-713i*wfu!)!9pn6ynh+=+ju
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["encheres-backend.herokuapp.com", "127.0.0.1"]
 
@@ -176,14 +176,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images')
 
 
 # Default primary key field type
