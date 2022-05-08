c81284b4362e813cb14a60138612d84c62ed88f1
Bk201li <bk201li@protonmail.com>
2022-05-09 00:44:22 +0200

    auction middleware done

--- encheres/middleware/auction_middleware.py
+++ encheres/middleware/auction_middleware.py
@@ -1,9 +1,17 @@
-def simple_middleware(get_response):
+def auction_middleware(get_response):
     # One-time configuration and initialization.
 
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
+        from api.models import Auction
+        from datetime import datetime, timedelta
+
+        auctions = Auction.objects.filter(end=False)
+        for auction in auctions:
+            if datetime.now() - auction.updated_at > (datetime.now() - auction.updated_at) + timedelta(minutes=60):
+                auction.end = True
+                auction.save()
 
         response = get_response(request)
 
